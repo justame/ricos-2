5d43babc02b217aeebe79680ea2c865225c61a8d
Dana Rish <45333005+danarish4@users.noreply.github.com>
2022-08-14 14:58:39 +0300

    [tiptap-button] fix width on resize (#4372)

--- packages/plugin-button/web/src/tiptap/component.tsx
+++ packages/plugin-button/web/src/tiptap/component.tsx
@@ -5,12 +5,14 @@ import { RicosContext } from 'ricos-context';
 
 export const Button: React.FC<PluginProps> = ({ componentData, settings }) => {
   const { theme, isMobile } = useContext(RicosContext);
+  const style = { width: componentData.config?.width };
   return (
     <ButtonComponent
       componentData={componentData}
       settings={settings}
       isMobile={isMobile}
       theme={theme}
+      style={style}
       helpers={{}}
     />
   );
