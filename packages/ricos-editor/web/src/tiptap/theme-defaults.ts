57d8c88b146219a1289c358912ad0646641dafb6
Aviv Maimon <maimonav@post.bgu.ac.il>
2022-08-24 13:24:41 +0300

    [ricos-editor] internal: add theme default values (#4459)

--- packages/ricos-editor/web/src/tiptap/theme-defaults.ts
+++ packages/ricos-editor/web/src/tiptap/theme-defaults.ts
@@ -1,26 +1,40 @@
 // reference for values is in consts.scss in common package
-export const THEME_DEFAULTS = {
+import type { RicosCustomStyles } from 'ricos-types';
+
+const defaultThemeValues = { fontWeight: 'normal', fontStyle: 'normal', lineHeight: '1.5' };
+
+export const THEME_DEFAULTS: {
+  desktop: { customStyles: RicosCustomStyles };
+  mobile: { customStyles: RicosCustomStyles };
+} = {
   desktop: {
     customStyles: {
       p: {
+        ...defaultThemeValues,
         fontSize: '16px',
       },
       h1: {
+        ...defaultThemeValues,
         fontSize: '80px',
       },
       h2: {
+        ...defaultThemeValues,
         fontSize: '28px',
       },
       h3: {
+        ...defaultThemeValues,
         fontSize: '24px',
       },
       h4: {
+        ...defaultThemeValues,
         fontSize: '20px',
       },
       h5: {
+        ...defaultThemeValues,
         fontSize: '18px',
       },
       h6: {
+        ...defaultThemeValues,
         fontSize: '16px',
       },
     },
@@ -28,24 +42,31 @@ export const THEME_DEFAULTS = {
   mobile: {
     customStyles: {
       p: {
+        ...defaultThemeValues,
         fontSize: '16px',
       },
       h1: {
+        ...defaultThemeValues,
         fontSize: '32px',
       },
       h2: {
+        ...defaultThemeValues,
         fontSize: '24px',
       },
       h3: {
+        ...defaultThemeValues,
         fontSize: '20px',
       },
       h4: {
+        ...defaultThemeValues,
         fontSize: '20px',
       },
       h5: {
+        ...defaultThemeValues,
         fontSize: '16px',
       },
       h6: {
+        ...defaultThemeValues,
         fontSize: '14px',
       },
     },
