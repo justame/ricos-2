a0cf527a75e876dc0395f9e969cb274d8cd5d85d
Dana Rish <45333005+danarish4@users.noreply.github.com>
2022-09-05 14:48:29 +0300

    [tiptap-html] fix html custom height (#4571)

--- packages/plugin-html/web/src/tiptap/component.tsx
+++ packages/plugin-html/web/src/tiptap/component.tsx
@@ -3,28 +3,13 @@ import { Component as HtmlComponent } from '../HtmlComponent';
 import type { PluginProps } from 'ricos-types';
 import { RicosContext } from 'ricos-context';
 
-export const Html: React.FC<PluginProps> = ({
-  settings,
-  componentData,
-  updateAttributes,
-  node,
-}) => {
+export const Html: React.FC<PluginProps> = ({ settings, componentData }) => {
   const { theme, isMobile, experiments } = useContext(RicosContext);
-
-  const updateHtmlHeight = data => {
-    updateAttributes({
-      containerData: { ...node.attrs.containerData, height: { custom: data.config.height } },
-    });
-  };
-  const store = {
-    update: (type, data) => updateHtmlHeight(data),
-  };
 
   return (
     <HtmlComponent
       componentData={componentData}
       iframeSandboxDomain={''}
-      store={store}
       settings={settings}
       isMobile={isMobile}
       theme={theme}
