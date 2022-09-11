7b86d97aa40c71ba7f7aa7d9114d2ab01ca17be4
yaron nachshon <yaronn@wix.com>
2022-09-11 06:09:11 -0700

    (fix): drag and drop - collapsible list drop handler (#4605)

--- packages/plugin-collapsible-list/web/src/tiptap/dropHandlerPlugin.ts
+++ packages/plugin-collapsible-list/web/src/tiptap/dropHandlerPlugin.ts
@@ -1,15 +1,30 @@
-import { TIPTAP_COLLAPSIBLE_LIST_TYPE } from 'ricos-content';
+import { TIPTAP_COLLAPSIBLE_ITEM_BODY_TYPE, TIPTAP_COLLAPSIBLE_ITEM_TYPE } from 'ricos-content';
 import { collapsibleItemDropHandlerPlugin } from '../consts';
 
 export const dropHandlerPlugin = (editor, Plugin) => {
   return new Plugin({
     key: collapsibleItemDropHandlerPlugin,
     props: {
-      handleDrop(view, event) {
-        const coords = { left: event.clientX, top: event.clientY };
-        const pos = view.posAtCoords(coords);
-        const path = editor.state.doc.resolve(pos.pos).path;
-        return !path.some(node => node?.type?.name === TIPTAP_COLLAPSIBLE_LIST_TYPE);
+      handleDrop(view, event, slice, moved) {
+        const collapsibleItemFound = slice?.content?.content.find(
+          node => node.type.name === TIPTAP_COLLAPSIBLE_ITEM_TYPE
+        );
+        if (moved && collapsibleItemFound) {
+          const coords = { left: event.clientX, top: event.clientY };
+          const pos = view.posAtCoords(coords);
+          const path = editor.state.doc.resolve(pos.pos);
+          const parent = path.parent;
+
+          if (
+            parent.type.name === TIPTAP_COLLAPSIBLE_ITEM_BODY_TYPE ||
+            parent.type.name === TIPTAP_COLLAPSIBLE_ITEM_TYPE
+          ) {
+            return false;
+          }
+
+          return true;
+        }
+        return false;
       },
     },
   });
